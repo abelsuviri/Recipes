72f879d6ac8a7f9ece58cd63dde4764d1136e321
Abel Suviri <abel.suviri.payan@gmail.com>
2020-05-17 19:12:43 +0100

    Create service

---
+++ data/src/main/java/com/abelsuviri/data/network/MealService.kt
@@ -0,0 +1,21 @@
+package com.abelsuviri.data.network
+
+import com.abelsuviri.data.model.CategoriesList
+import com.abelsuviri.data.model.MealsList
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * @author Abel Suviri
+ */
+
+interface MealService {
+    @GET("api/json/v1/1/categories.php")
+    suspend fun getCategories(): Deferred<CategoriesList>
+
+    @GET("api/json/v1/1/filter.php")
+    suspend fun getMeals(
+        @Query("c") category: String
+    ): Deferred<MealsList>
+}
